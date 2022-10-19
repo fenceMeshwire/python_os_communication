97ffb9c54a72273217055892588384b30158604e
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-10-19 15:52:24 +0200

    Update 00_introduction.py

--- 00_introduction.py
+++ 00_introduction.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python
 
+# Python 3.9.5
+
+# 00_introduction.py
+
+# Dependency
+import subprocess
+
+path = 'C:\\Windows\\System32\\calc.exe'
+
+process = subprocess.Popen(path, shell=True)
